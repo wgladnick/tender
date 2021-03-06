61f90b19f42ed3006e64c794757c7397731579c9
Michael Uhlman <muhlman@gmail.com>
2021-03-05 20:35:03 -0500

    fixed a typo in script

--- java/database/alterusertable.sql
+++ java/database/alterusertable.sql
@@ -2,15 +2,10 @@ SELECT * FROM users;
 
 START TRANSACTION;
 
-
-
-
 ALTER TABLE users
 
-ADD COLUMN first_name VARCHAR NOT NULL,
-ADD COLUMN last_name VARCHAR NOT NULL;
-
-SELECT * FROM users;
+ADD COLUMN first_name VARCHAR,
+ADD COLUMN last_name VARCHAR;
 
 UPDATE users
 SET first_name = 'user', last_name = 'name'
@@ -19,13 +14,12 @@ WHERE user_id = 1;
 UPDATE users
 SET first_name = 'admin', last_name = 'istrator'
 WHERE user_id = 2;
+
 ALTER TABLE users
 
 ALTER COLUMN first_name SET NOT NULL,
 ALTER COLUMN last_name SET NOT NULL;
 
-
+SELECT * FROM users;
 
 COMMIT;
-
-
