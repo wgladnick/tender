533c2e7ab4db71fdaf266eed8da1b06a8358b1b9
William Gladnick <wgladnick@gmail.com>
2021-03-07 17:30:41 -0500

    added some models for invitation portion

--- java/src/main/java/com/techelevator/model/invitation/Invitation.java
+++ java/src/main/java/com/techelevator/model/invitation/Invitation.java
@@ -1,5 +1,86 @@
 package com.techelevator.model.invitation;
 
-public class Invitation {
+import java.security.Timestamp;
 
+public class Invitation {
+	private Long inviteId;
+	private String location;
+	private int radius;
+	private int creatorId;
+	private Timestamp deadline;
+	private String reservationDate;
+	private Invitee[] invitees;
+	private InviteRestaurants[] restaurantChoices;
+	
+	public Invitation() {
+		
+	}
+
+	public Long getInviteId() {
+		return inviteId;
+	}
+
+	public void setInviteId(Long inviteId) {
+		this.inviteId = inviteId;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public Integer getCreatorId() {
+		return creatorId;
+	}
+
+	public void setCreatorId(int creatorId) {
+		this.creatorId = creatorId;
+	}
+
+	public Timestamp getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Timestamp deadline) {
+		this.deadline = deadline;
+	}
+
+	public String getReservationDate() {
+		return reservationDate;
+	}
+
+	public void setReservationDate(String reservationDate) {
+		this.reservationDate = reservationDate;
+	}
+
+	public Invitee[] getInvitees() {
+		return invitees;
+	}
+
+	public void setInvitees(Invitee[] invitees) {
+		this.invitees = invitees;
+	}
+
+	public InviteRestaurants[] getRestaurantChoices() {
+		return restaurantChoices;
+	}
+
+	public void setRestaurantChoices(InviteRestaurants[] restaurantChoices) {
+		this.restaurantChoices = restaurantChoices;
+	}
+	
+	
+	
 }
+
