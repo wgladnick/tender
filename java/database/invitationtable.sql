c49ed0fda0187dab565bac9f53a6c24f9c0fceb4
William Gladnick <wgladnick@gmail.com>
2021-03-07 16:40:13 -0500

    invitation table in sql

---
+++ java/database/invitationtable.sql
@@ -0,0 +1,44 @@
+START TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS invitation (
+                invite_id SERIAL PRIMARY KEY,
+                location VARCHAR NOT NULL,
+                radius VARCHAR NOT NULL,
+                creator_user_id VARCHAR NOT NULL,
+                deadline TIMESTAMPTZ NOT NULL,
+                reservation_date_time VARCHAR NOT NULL
+                );
+              
+CREATE TABLE IF NOT EXISTS invitee_details (
+                invite_id INTEGER,
+                unique_id VARCHAR NOT NULL,
+                user_id VARCHAR NOT NULL,
+                first_name VARCHAR NOT NULL,
+                last_name VARCHAR NOT NULL,
+                email VARCHAR NOT NULL,
+                has_voted BOOLEAN,
+                is_attending BOOLEAN DEFAULT FALSE,
+                
+                 CONSTRAINT fk_invite FOREIGN KEY(invite_id) REFERENCES invitation (invite_id)
+                
+                );
+                
+CREATE TABLE IF NOT EXISTS invitation_votes (
+                invite_id INTEGER,
+                yelp_id VARCHAR NOT NULL,
+                thumbs_up INTEGER,
+                thumbs_down INTEGER,
+                
+                 CONSTRAINT fk_invite FOREIGN KEY(invite_id) REFERENCES invitation (invite_id)
+                );
+                      
+              
+COMMIT;             
+                
+        ]
+                              
+                
+
+                
+                
+                
