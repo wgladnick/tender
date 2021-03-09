198f4c9e32b35ece7371591e61a597740745b99c
Michael Uhlman <muhlman@gmail.com>
2021-03-09 16:13:22 -0500

    invitee fixes and db cleanup
    Signed-off-by: Michael Uhlman <muhlman@gmail.com>

--- java/database/addfavoritestable.sql
+++
@@ -1,8 +0,0 @@
-START TRANSACTION;
-
-CREATE TABLE IF NOT EXISTS favorites (
-    user_id INTEGER NOT NULL,
-    business_id VARCHAR NOT NULL
-);
-    
-COMMIT;
--- java/src/main/java/com/techelevator/dao/invitation/InviteeSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/invitation/InviteeSqlDAO.java
@@ -11,82 +11,86 @@ import java.util.List;
 import java.util.Random;
 
 @Component
-public class InviteeSqlDAO implements InviteeDAO{
-	
-	private JdbcTemplate jdbcTemplate;
-	
-	public InviteeSqlDAO(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate =jdbcTemplate;
-	}
+public class InviteeSqlDAO implements InviteeDAO {
 
-	@Override
-	public List<Invitee> getInviteeById(Long inviteId) {
-		List<Invitee> invitees = new ArrayList<>();
-	 String sql = "SELECT * FROM invitee_details WHERE invite_id = ?";
-	
-	SqlRowSet results = jdbcTemplate.queryForRowSet(sql, inviteId);
-	
-	while(results.next()) {
-		Invitee invitee = mapRowToInvitee(results);
-		invitees.add(invitee);
-	}
-	return invitees;
-	}
+    private JdbcTemplate jdbcTemplate;
 
-	@Override
-	public Invitee createInvitee(Invitee invitee) {
-		// Generates the unique ID we will use on the front end to track the users.
-		String uniqueId = generateUniqueId();
-		invitee.setUniqueId(uniqueId);
+    public InviteeSqlDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-		String sql = "INSERT INTO invitee_details (invite_id,unique_id, invitee_user_id, first_name, last_name, email) VALUES (?,?,?,?,?,?) RETURNING has_voted, is_attending";
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sql,invitee.getInviteId(),invitee.getUniqueId(),invitee.getUserId(),
-				invitee.getFirstName(), invitee.getLastName(),invitee.getEmail());
+    @Override
+    public List<Invitee> getInviteeById(Long inviteId) {
+        List<Invitee> invitees = new ArrayList<>();
+        String sql = "SELECT * FROM invitee_details WHERE invite_id = ?";
 
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, inviteId);
+
+        while (results.next()) {
+            Invitee invitee = mapRowToInvitee(results);
+            invitees.add(invitee);
+        }
+        return invitees;
+    }
+
+    @Override
+    public Invitee createInvitee(Invitee invitee) {
+        // Generates the unique ID we will use on the front end to track the users.
+        String uniqueId = generateUniqueId();
+        invitee.setUniqueId(uniqueId);
+
+        String sql = "INSERT INTO invitee_details (invite_id,unique_id, invitee_user_id, first_name, last_name, email) VALUES (?,?,?,?,?,?) RETURNING has_voted, is_attending";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, invitee.getInviteId(), invitee.getUniqueId(), invitee.getUserId(),
+                invitee.getFirstName(), invitee.getLastName(), invitee.getEmail());
+
+        while (results.next()) {
+            invitee.setHasVoted(results.getBoolean("has_voted"));
+            invitee.setIsAttending(results.getString("is_attending"));
+        }
+
+        return invitee;
+    }
+
+    @Override
+    public Invitee updateInviteeStatus(Invitee invitee) {
+
+        String sql = "UPDATE invitee_details SET has_voted = ?, is_attending = ? WHERE unique_id = ?";
+
+        jdbcTemplate.update(sql, invitee.getHasVoted(), invitee.getIsAttending(), invitee.getUniqueId());
+		SqlRowSet results = jdbcTemplate.queryForRowSet("SELECT * FROM invitee_details WHERE unique_id = ?", invitee.getUniqueId());
 		while (results.next()) {
-			invitee.setHasVoted(results.getBoolean("has_voted"));
-			invitee.setIsAttending(results.getString("is_attending"));
+			invitee = mapRowToInvitee(results);
 		}
+        return invitee;
+    }
 
-		return invitee;
-	}
+    private Invitee mapRowToInvitee(SqlRowSet rs) {
+        Invitee invitee = new Invitee();
 
-	@Override
-	public Invitee updateInviteeStatus(Invitee invitee) {
+        invitee.setInviteId(rs.getInt("invite_id"));
+        invitee.setUniqueId(rs.getString("unique_id"));
+        invitee.setUserId(rs.getInt("invitee_user_id"));
+        invitee.setFirstName(rs.getString("first_name"));
+        invitee.setLastName(rs.getString("last_name"));
+        invitee.setEmail(rs.getString("email"));
+        invitee.setHasVoted(rs.getBoolean("has_voted"));
+        invitee.setIsAttending(rs.getString("is_attending"));
 
-		String sql = "UPDATE invitee_details SET has_voted = ?, is_attending = ? WHERE unique_id = ?";
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sql,invitee.getHasVoted(), invitee.getIsAttending(), invitee.getUniqueId());
-
-		return invitee;
-	}
-	
-	private Invitee mapRowToInvitee(SqlRowSet rs) {
-		Invitee invitee = new Invitee();
-		
-		invitee.setInviteId(rs.getInt("invite_id"));
-		invitee.setUniqueId(rs.getString("unique_id"));
-		invitee.setUserId(rs.getInt("invitee_user_id"));
-		invitee.setFirstName(rs.getString("first_name"));
-		invitee.setLastName(rs.getString("last_name"));
-		invitee.setEmail(rs.getString("email"));
-		invitee.setHasVoted(rs.getBoolean("has_voted"));
-		invitee.setIsAttending(rs.getString("is_attending"));
-		
-		return invitee;
-	}
+        return invitee;
+    }
 
 
-	private String generateUniqueId() {
-		int leftLimit = 48; // numeral '0'
-		int rightLimit = 122; // letter 'z'
-		int targetStringLength = 15;
-		Random random = new Random();
+    private String generateUniqueId() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 15;
+        Random random = new Random();
 
-		return random.ints(leftLimit, rightLimit + 1)
-				.filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-				.limit(targetStringLength)
-				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-				.toString();
-	}
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
 
 }
