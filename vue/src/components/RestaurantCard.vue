88394b1c7ff70242c3c6b9f5e54bfebc405e816d
Emily Charlson <elcharlson1107@gmail.com>
2021-03-10 17:34:27 -0500

    card content complete

--- vue/src/components/RestaurantCard.vue
+++ vue/src/components/RestaurantCard.vue
@@ -19,16 +19,33 @@
         v-for="ctg in restaurant.categories"
         v-bind:key="ctg.title"
       >
-
         {{ ctg.title }}
       </b-button>
     </div>
-    <p
+    <span
       v-for="ln in restaurant.location.display_address"
       v-bind:key="ln.display_address"
     >
       {{ ln }}
-    </p>
+    </span>
+    <p>{{ restaurant.display_phone }}</p>
+    <span>
+      <a :href="`tel:${restaurant.phone}`">
+        <b-button rounded size="is-small">
+          <i class="fas fa-phone-alt"></i>
+          Call to order</b-button
+        >
+      </a>
+    </span>
+    <div>
+      <b-button
+        size="is-small"
+        rounded
+        v-for="transaction in restaurant.transactions"
+        v-bind:key="transaction.label"
+        >{{ transaction.label }}</b-button
+      >
+    </div>
     <img :src="restaurant.image_url" class="yelp-image" />
   </div>
 </template>
