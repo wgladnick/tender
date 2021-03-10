1c50312b39947b2a7ed49e1e84644cc316fbf67a
Emily Charlson <elcharlson1107@gmail.com>
2021-03-10 13:05:22 -0500

    card in progress

--- vue/src/components/RestaurantCard.vue
+++ vue/src/components/RestaurantCard.vue
@@ -2,18 +2,26 @@
   <div>
 
      <h1>{{restaurant.name}}</h1>
-     <star-rating></star-rating>
-     <p>{{restaurant.category}}</p>
-     <p>{{restaurant.location.display_address[0]}}</p>
+     <star-rating  />
+     <p v-for="ctg in restaurant.categories" 
+        v-bind:key="ctg.title">{{ctg.title}}</p>
+     <p v-for="ln in restaurant.location.display_address" v-bind:key="ln.display_address">
+       {{ln}}
+       </p>
      <img :src="restaurant.image_url" class="yelp-image" />
      
   </div>
 </template>
 
 <script>
-
+import StarRating from "vue-star-rating";
 export default {
+
+
   name: 'restaurant-card',
+  components: {
+    StarRating
+  },
   props: ["restaurant"]
 
 }
