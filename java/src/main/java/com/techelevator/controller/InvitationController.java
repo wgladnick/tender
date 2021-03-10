84f89a0bd2b088bdc0dbb4013d873bcd633cd250
Michael Uhlman <muhlman@gmail.com>
2021-03-09 20:43:46 -0500

    more cleanup
    Signed-off-by: Michael Uhlman <muhlman@gmail.com>

--- java/src/main/java/com/techelevator/controller/InvitationController.java
+++ java/src/main/java/com/techelevator/controller/InvitationController.java
@@ -3,12 +3,8 @@ package com.techelevator.controller;
 import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.techelevator.dao.invitation.InvitationDAO;
 import com.techelevator.dao.invitation.InviteRestaurantsDAO;
@@ -18,8 +14,7 @@ import com.techelevator.model.invitation.InviteRestaurants;
 import com.techelevator.model.invitation.Invitee;
 
 @RestController
-@ComponentScan({"com.techelevator.dao"})
-@CrossOrigin 
+@CrossOrigin
 @RequestMapping(value = "/invitation")
 public class InvitationController {
 	
@@ -32,20 +27,18 @@ public class InvitationController {
 		this.inviteeDAO = inviteeDAO;
 		this.inviteRestaurantsDAO = inviteRestaurantsDAO;
 	}
-	
-	
+
 	@RequestMapping(value = "/{inviteId}", method = RequestMethod.GET)
 	public Invitation getInviteById(@PathVariable Long inviteId) {
 		return invitationDAO.getInviteById(inviteId);
 	}
-	
-	
+
 	@RequestMapping(value = "/user/{creatorId}", method = RequestMethod.GET)
 	public List<Invitation> findInviteByUserId(@PathVariable int creatorId) {
 		return invitationDAO.findInviteByUserId(creatorId);
 	}
-	
-	
+
+	@ResponseStatus(HttpStatus.CREATED)
 	@RequestMapping(method = RequestMethod.POST)
 	public Invitation create(@RequestBody Invitation invitation) {
 		return invitationDAO.create(invitation);
@@ -55,31 +48,24 @@ public class InvitationController {
 	public List<Invitee> getInviteeById(@PathVariable Long inviteId) {
 		return inviteeDAO.getInviteeById(inviteId);
 	}
-	
-	
+
 	@RequestMapping(value = "/invitee", method = RequestMethod.PUT)
 	public Invitee updateInviteeStatus(@RequestBody Invitee invitee) {
 		return inviteeDAO.updateInviteeStatus(invitee);
 	}
-	
-	
+
 	@RequestMapping(value = "/restaurant/{inviteId}", method = RequestMethod.GET)
 	public List<InviteRestaurants> getInviteRestaurantById(@PathVariable Long inviteId) {
 		return inviteRestaurantsDAO.getInviteRestaurantById(inviteId);
 	}
-	
-	
+
 	@RequestMapping(value = "/thumbsup", method = RequestMethod.PUT)
 	public InviteRestaurants voteThumbsUp(@RequestBody InviteRestaurants inviterestaurants) {
 		return inviteRestaurantsDAO.voteThumbsUp(inviterestaurants);
 	}
-	
-	
+
 	@RequestMapping(value = "/thumbsdown", method = RequestMethod.PUT)
 	public InviteRestaurants voteThumbsDown(@RequestBody InviteRestaurants inviterestaurants) {
 		return inviteRestaurantsDAO.voteThumbsDown(inviterestaurants);
 	}
-	
-	
-	
 }
