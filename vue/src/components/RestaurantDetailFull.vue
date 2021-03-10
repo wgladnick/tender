09562f3c3d78177789874e54adf1c4e1e47de2b4
Michael Uhlman <muhlman@gmail.com>
2021-03-10 10:24:06 -0500

    restaurant details work
    Signed-off-by: Michael Uhlman <muhlman@gmail.com>

--- vue/src/components/RestaurantDetailFull.vue
+++ vue/src/components/RestaurantDetailFull.vue
@@ -1,30 +1,79 @@
 <template>
-<div>
-    {{restaurant.name}}
+  <div>
+    <h1>{{ restaurant.name }}</h1>
+    <b-button
+      v-bind:type="{
+        'is-danger': !restaurant.hours.is_open_now,
+        'is-success': !restaurant.hours.is_open_now,
+      }"
+    >
+      <span v-if="!restaurant.hours.is_open_now">Currently Closed</span>
+      <span v-else>Currently Open</span>
+    </b-button>
+    <br />
+    <b-button
+      rounded
+      v-for="category in restaurant.categories"
+      v-bind:key="category.title"
+      >{{ category.title }}</b-button
+    >
+    <br>
+    <b-button
+      rounded
+      v-for="transaction in restaurant.transactions"
+      v-bind:key="transaction.title"
+      >{{ transaction.label }}</b-button
+    >
+
+
+    <star-rating
+      :rating="restaurant.rating"
+      read-only="true"
+      increment="0.01"
+      show-rating="false"
+    ></star-rating>
+    <div>
+      <span
+        v-for="line in restaurant.location.display_address"
+        v-bind:key="line"
+      >
+        {{ line }}
+        <br />
+      </span>
+      
+      <br />
+      <a :href="`tel:${restaurant.phone}`">
+      <b-button rounded>Call to order</b-button>
+      </a>
     </div>
-  
+    <img v-bind:src="restaurant.image_url" width="150px" />
+  </div>
 </template>
 
 <script>
-import RestaurantService from '../services/RestaurantService'
+import RestaurantService from "../services/RestaurantService";
+import StarRating from "vue-star-rating";
+
 export default {
-        data() {
+  components: {
+    StarRating,
+  },
+  data() {
     return {
       restaurant: {},
       isLoading: true,
     };
   },
   created() {
-      RestaurantService.getTheRestaurant(this.$route.params.id).then((response) => {
-      this.restaurant = response.data;
-    });
-    this.isLoading = false;
-    console.log('Details: ' + this.restaurant);
+    RestaurantService.getTheRestaurant(this.$route.params.id).then(
+      (response) => {
+        this.restaurant = response.data;
+        this.isLoading = false;
+      }
+    );
   },
-
-}
+};
 </script>
 
 <style>
-
 </style>
