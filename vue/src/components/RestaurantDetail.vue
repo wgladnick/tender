68392769756646e9e74771f6165e39d4a15ec2b6
Emily Charlson <elcharlson1107@gmail.com>
2021-03-11 16:51:48 -0500

    restaurant details progress

--- vue/src/components/RestaurantDetail.vue
+++ vue/src/components/RestaurantDetail.vue
@@ -1,104 +1,144 @@
 <template>
-    <div v-if="!isLoading">
-    <h1>{{ restaurant.name }}</h1>
-    <p>Price: {{restaurant.price}}</p>
+  <main>
+    <div class="main-div" v-if="!isLoading">
+      <div class="left-panel">
+        <img
+          class="yelp-image"
+          v-bind:src="restaurant.image_url"
+        />
+      </div>
+      <div class="right-panel">
+        <h1 class="title">{{ restaurant.name }}</h1>
 
-    <p v-for="hours in restaurant.hours"
-      v-bind:key="hours.hours_type">
-    <b-button 
-      v-bind:type="{
-        'is-danger': !hours.is_open_now,
-        'is-success': hours.is_open_now
-      }"
-    >
-      <span v-if="!hours.is_open_now">Currently Closed</span>
-      <span v-else>Currently Open</span>
-    </b-button>
-    </p>
+        <p class="price">Price: {{ restaurant.price }}</p>
 
-    
-    <br />
-    <b-button
-    size="is-small"
-      rounded
-      v-for="category in restaurant.categories"
-      v-bind:key="category.title"
-      >{{ category.title }}</b-button
-    >
-    <br>
-      <b-button size="is-small"
-      >
-      <i class="fas fa-check has-text-success"
-      v-if="transactionTypes.includes('pickup')"></i>
-      <i class="fas fa-times has-text-danger"
-      v-if="!transactionTypes.includes('pickup')"></i>
-      Takeout</b-button
-    >
-     <b-button size="is-small"
-      >
-       <i class="fas fa-check has-text-success"
-      v-if="transactionTypes.includes('delivery')"></i>
-      <i class="fas fa-times has-text-danger"
-      v-if="!transactionTypes.includes('delivery')"></i>
-      Delivery</b-button
-    >
-     <b-button size="is-small"
-      >
-       <i class="fas fa-check has-text-success"
-      v-if="transactionTypes.includes('restaurant_reservation')"></i>
-      <i class="fas fa-times has-text-danger"
-      v-if="!transactionTypes.includes('restaurant_reservation')"></i>
-      Reservations</b-button
-    >
-    <br>
-<div>
-    <star-rating
-      :rating="restaurant.rating"
-      :read-only="true"
-      :increment="0.5"
-      :show-rating="false"
-    />
-    {{restaurant.review_count}} reviews
-</div>
+        <p v-for="hours in restaurant.hours" v-bind:key="hours.hours_type">
+          <b-button
+            v-bind:type="{
+              'is-danger': !hours.is_open_now,
+              'is-success': hours.is_open_now,
+            }"
+          >
+            <span v-if="!hours.is_open_now">Currently Closed</span>
+            <span v-else>Currently Open</span>
+          </b-button>
+        </p>
 
-    <div>
-      <span
-        v-for="line in this.restaurant.location.display_address"
-        v-bind:key="line"
-      >
-        {{ line }}
         <br />
-      </span>
-      <a :href="`https://www.google.com/maps/dir/19801/${ restaurant.location.display_address.toString().split(' ').join('+')}/`" target="_blank">
-      <b-button rounded size="is-small">
-          <i class="fas fa-route"></i>
-Get Directions</b-button>
-      </a>
-      <p>{{ restaurant.display_phone }}</p>
-      <a :href="`tel:${restaurant.phone}`">
-      <b-button rounded size="is-small">
-          <i class="fas fa-phone-alt"></i>
-Call to order</b-button>
-      </a>
+        <b-button
+          size="is-small"
+          rounded
+          v-for="category in restaurant.categories"
+          v-bind:key="category.title"
+          >{{ category.title }}</b-button
+        >
+        <br />
+        <div class="transactions">
+          <b-button size="is-small">
+            <i
+              class="fas fa-check has-text-success"
+              v-if="transactionTypes.includes('pickup')"
+            ></i>
+            <i
+              class="fas fa-times has-text-danger"
+              v-if="!transactionTypes.includes('pickup')"
+            ></i>
+            Takeout</b-button
+          >
+          <b-button size="is-small">
+            <i
+              class="fas fa-check has-text-success"
+              v-if="transactionTypes.includes('delivery')"
+            ></i>
+            <i
+              class="fas fa-times has-text-danger"
+              v-if="!transactionTypes.includes('delivery')"
+            ></i>
+            Delivery</b-button
+          >
+          <b-button size="is-small">
+            <i
+              class="fas fa-check has-text-success"
+              v-if="transactionTypes.includes('restaurant_reservation')"
+            ></i>
+            <i
+              class="fas fa-times has-text-danger"
+              v-if="!transactionTypes.includes('restaurant_reservation')"
+            ></i>
+            Reservations</b-button
+          >
+        </div>
+        <br />
+        <div>
+          <star-rating
+            :rating="restaurant.rating"
+            :read-only="true"
+            :increment="0.5"
+            :show-rating="false"
+          />
+          {{ restaurant.review_count }} reviews
+        </div>
 
+        <div>
+          <span
+            v-for="ln in restaurant.location.display_address"
+            v-bind:key="ln.display_address"
+          >
+            {{ ln }}
+          </span>
+          <a
+            :href="`https://www.google.com/maps/dir/19801/${restaurant.location.display_address
+              .toString()
+              .split(' ')
+              .join('+')}/`"
+            target="_blank"
+          >
+            <b-button rounded size="is-small">
+              <i class="fas fa-route"></i>
+              Get Directions</b-button
+            >
+          </a>
+          <br />
+          <span>{{ restaurant.display_phone }} </span>
+          <span>
+            <a :href="`tel:${restaurant.phone}`">
+              <b-button type="is-primary" rounded size="is-small">
+                <i class="fas fa-phone-alt"></i>
+                Call to order</b-button
+              >
+            </a>
+          </span>
 
-      <p>Hours:</p>
-      <p v-for="hours in restaurant.hours"
-      v-bind:key="hours.hours_type">
-      <span v-for="open in hours.open"
-      v-bind:key="open.day">
-      <span v-if="open.day === 6">Sunday: {{open.start}} - {{open.end}}<br></span> 
-      <span v-if="open.day === 0">Monday: {{open.start}} - {{open.end}}<br></span> 
-      <span v-if="open.day === 1">Tuesday: {{open.start}} - {{open.end}}<br></span> 
-      <span v-if="open.day === 2">Wednesday: {{open.start}} - {{open.end}}<br></span> 
-      <span v-if="open.day === 3">Thursday: {{open.start}} - {{open.end}}<br></span> 
-      <span v-if="open.day === 4">Friday: {{open.start}} - {{open.end}}<br></span> 
-      <span v-if="open.day === 5">Saturday: {{open.start}} - {{open.end}}<br></span> 
-      </span>
-      </p>
+          <p>Hours:</p>
+          <p v-for="hours in restaurant.hours" v-bind:key="hours.hours_type">
+            <span v-for="open in hours.open" v-bind:key="open.day">
+              <span v-if="open.day === 6"
+                >Sunday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+              <span v-if="open.day === 0"
+                >Monday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+              <span v-if="open.day === 1"
+                >Tuesday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+              <span v-if="open.day === 2"
+                >Wednesday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+              <span v-if="open.day === 3"
+                >Thursday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+              <span v-if="open.day === 4"
+                >Friday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+              <span v-if="open.day === 5"
+                >Saturday: {{ open.start }} - {{ open.end }}<br
+              /></span>
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
-    <img v-bind:src="restaurant.image_url" width="150px" />
-  </div>
+  </main>
 </template>
 
 <script>
@@ -115,7 +155,7 @@ export default {
       restaurant: {},
       isLoading: true,
       directionsAddress: [],
-      transactionTypes: ''
+      transactionTypes: "",
     };
   },
   created() {
@@ -125,15 +165,45 @@ export default {
         this.isLoading = false;
 
         for (let i = 0; i < this.restaurant.transactions.length; i++) {
-          this.transactionTypes += this.restaurant.transactions[i].transactions + " ";
+          this.transactionTypes +=
+            this.restaurant.transactions[i].transactions + " ";
         }
       }
     );
   },
-
 };
 </script>
 
-<style>
+<style scoped>
+.main-div {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+}
+.yelp-image {
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+}
+.left-panel {
+  padding: 20px;
+  margin-right: 25px;
+}
+.right-panel {
+  padding: 20px;
+ 
+}
+.button.is-rounded {
+  border-radius: 290486px;
+  padding-left: calc(1em + 0.25em);
+  margin-right: 10px;
+  padding-right: calc(1em + 0.25em);
+}
+.price {
+  font-weight: 550;
+}
 
+.transactions > button.is-small {
+  border: none;
+}
 </style>
