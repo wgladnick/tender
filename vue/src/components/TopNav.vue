a786d6d8f92bd56f606750b269c4ddf1ab1cb766
Joshua Bannen <josh.bannen@gmail.com>
2021-03-12 15:25:50 -0500

    updated logos

--- vue/src/components/TopNav.vue
+++ vue/src/components/TopNav.vue
@@ -1,58 +1,62 @@
 <template>
-<div class="header" >
-      <div class="logo">
-         <router-link  class="nav" v-bind:to="{ name: 'restaurantFinder' }"><img class="flame" style="padding-right: 10px;" src="../assets/TenderFlame.png"/>
-         <img class="tender-text" src="../assets/TenderTextNoStroke.png" /></router-link>
-      </div>
-
-      <div id="nav-container">
-        <router-link v-if="$route.name!=='restaurantFinder'" class="nav" v-bind:to="{ name: 'restaurantFinder' }">Home</router-link
-        >&nbsp;&nbsp;
-        <router-link
-          class="nav"
-          v-bind:to="{ name: 'logout' }"
-          v-if="$store.state.token != ''"
-          >Logout</router-link
-        >
-      </div>
+  <div class="header">
+    <div class="logo">
+      <router-link class="nav" v-bind:to="{ name: 'restaurantFinder' }"
+        ><img
+          class="flame"
+          style="padding-right: 10px"
+          src="../assets/TenderFlame2.png" />
+        <img class="tender-text" src="../assets/TenderTextNew.png"
+      /></router-link>
     </div>
-  
+
+    <div id="nav-container">
+      <router-link
+        v-if="$route.name !== 'restaurantFinder'"
+        class="nav"
+        v-bind:to="{ name: 'restaurantFinder' }"
+        >Home</router-link
+      >&nbsp;&nbsp;
+      <router-link
+        class="nav"
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  
-
-}
+export default {};
 </script>
 
 <style scoped>
-.logo{
+.logo {
   display: flex;
+  flex-direction: row;
 }
-.flame{
-  height: 70px;
+.flame {
+  height: 90px;
+  align-items: flex-start;
 }
-.tender-text{
-  height: 100px;
+.tender-text {
+  height: 90px;
   align-items: flex-end;
 }
 
-#nav-container{
-  margin-top:2em;
-  padding-right:2em;
+#nav-container {
+  margin-top: 2em;
+  padding-right: 2em;
 }
-.nav{
-  font-size:1.2em;
+.nav {
+  font-size: 1.2em;
   text-transform: uppercase;
-  font-weight:bold;
-  color:#dc6b67;
-
+  font-weight: bold;
+  color: #dc6b67;
 }
 
-.nav:hover{
-  color:#3c0f49;
+.nav:hover {
+  color: #3c0f49;
 }
-
-
 </style>
