7c3976e46dae9a44f4bdd9181ed36efe97e69914
Michael Uhlman <muhlman@gmail.com>
2021-03-12 17:20:12 -0500

    script changes for server
    Signed-off-by: Michael Uhlman <muhlman@gmail.com>

--- java/database/DB-Changes.sql
+++ java/database/DB-Changes.sql
@@ -1,5 +1,3 @@
-createdb -U postgres $DATABASE
-
 DROP TABLE IF EXISTS user_details;
 DROP TABLE IF EXISTS user_categories;
 DROP TABLE IF EXISTS food_categories;
@@ -22,7 +20,6 @@ CREATE TABLE users (
 
 INSERT INTO users (username,password_hash,role,first_name,last_name,email) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER','user','name','user@user.com');
 INSERT INTO users (username,password_hash,role,first_name,last_name,email) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN','admin','istrator','admin@user.com');
-INSERT INTO users (username,password_hash,role,first_name,last_name,email) VALUES ('test','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN','admin','istrator','admin@user.com');
 
 SELECT * FROM users;
 
