e3e0b388d0228d1db24520b93c978e2150d7bb13
William Gladnick <wgladnick@gmail.com>
2021-03-15 12:12:19 -0400

    userprofile

--- vue/src/views/UserProfile.vue
+++ vue/src/views/UserProfile.vue
@@ -1,10 +1,15 @@
 <template>
-  
+
+  <user-profile />
 </template>
 
 <script>
-
+import UserProfile from '../components/UserProfile'
 export default {
+    name: "user-profile",
+   components: {
+       UserProfile
+   }
 
 }
 </script>
