33ea58315c1713cbc86f87822dc307806f2e31f2
Matavia Porter <mataviaporter@gmail.com>
2021-03-15 21:05:24 -0400

    Dinner Card Update

--- vue/src/components/DinnerCard.vue
+++ vue/src/components/DinnerCard.vue
@@ -5,17 +5,56 @@
       <b-field label="Dinner Name">
         <b-input v-model="dinnerInvite.inviteName" type="text" />
       </b-field>
+      <b-field id="reservationDate" label="Select Date and Time">
+        <b-datetimepicker
+          v-model="dinnerInvite.reservationDate"
+          placeholder="Click to select..."
+        >
+          <template #left>
+            <b-button
+              label="Now"
+              type="is-primary"
+              icon-left="clock"
+              @click="dinnerInvite.reservationDate = new Date()"
+            />
+          </template>
 
-       <div>
-    <input type="date" v-model="reservationDate" />
-    <input type="time" v-model="reservationTime" />
+          <template #right>
+            <b-button
+              label="Clear"
+              type="is-danger"
+              icon-left="close"
+              outlined
+              @click="dinnerInvite.reservationDate = null"
+            />
+          </template>
+        </b-datetimepicker>
+      </b-field>
+      <b-field id="deadline" label="Deadline for Dinner Invite">
+        <b-datetimepicker
+          v-model="dinnerInvite.deadline"
+          placeholder="Click to select..."
+        >
+          <template #left>
+            <b-button
+              label="Now"
+              type="is-primary"
+              icon-left="clock"
+              @click="dinnerInvite.deadline = new Date()"
+            />
+          </template>
 
-    <input type="date" v-model="deadlineDate" />
-    <input type="time" v-model="deadlineTime" />
-    <button v-on:click="sendInvitation">send</button>
-  </div>
-     
-    
+          <template #right>
+            <b-button
+              label="Clear"
+              type="is-danger"
+              icon-left="close"
+              outlined
+              @click="dinnerInvite.deadline = null"
+            />
+          </template>
+        </b-datetimepicker>
+      </b-field>
       <div id="email-container">
         <div>
           <b-field
@@ -30,14 +69,14 @@
               id="name"
               type="text"
               :label="invitee.label2"
-              v-model="invitee.name"
+              v-model="invitee.value2"
               placeholder="Enter friend's name:"
             />
             <b-input
               id="email"
               type="email"
               :label="invitee.label1"
-              v-model="invitee.email"
+              v-model="invitee.value1"
               placeholder="Enter friend's email:"
             />
               <button @click="remove(i)" class="delete" id="delete-invitee" />
@@ -45,10 +84,9 @@
           <div>
             <b-button @click="add()" id="add-invitee"> Add Invitee</b-button>
           </div>
-          <b-button v-on:click="createInvite">
+          <b-button type="submit">
             Create Dinner and Invite Friends</b-button
           >
-
         </div>
       </div>
     </div>
@@ -62,47 +100,33 @@ export default {
 
   data() {
     return {
-      reservationDate:"",
-      reservationTime: "",
-      deadlineDate:"",
-      deadlineTime:"",
       showWeekNumber: false,
       enableSeconds: false,
       hourFormat: undefined, // Browser locale
       locale: undefined, // Browser locale
       dinnerInvite: {
         inviteName: "",
-        deadline:"",
-        reservationDate: "",
+        reservationDate: new Date(),
+        deadline: new Date(),
         invitees: [],
-
-   
-    
+        location: "",
+        radius: "",
+        restaurantChoices: [],
       },
     };
   },
 
-  computed: {
-    reservationDateTime() {
-      this.reservationDate = this.reservationDate + " " + this.reservationTime;
-    },
-    deadlineDateTime() {
-      this.deadline = this.deadlineDate + " " + this.deadlineTime;
-    },
-  },
-
   methods: {
-    createInvite() {
-          this.$store.commit("CREATE_INVITATION", this.dinnerInvite);
-          console.log(this.$store.state.invitation);
-      
-    
+    addInviteesToDinner() {
+      InviteService.sendInvite(this.dinnerInvite).then((response) => {
+        this.dinnerInvite = response.data;
+      });
     },
     add() {
       this.dinnerInvite.invitees.push({
-
+        label1: "email",
         value1: "",
-  
+        label2: "name",
         value2: "",
       });
     },
