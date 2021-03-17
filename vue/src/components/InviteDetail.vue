942496d27e1b31126b6c66216c1f47863d17478a
Joshua Bannen <josh.bannen@gmail.com>
2021-03-17 13:35:01 -0400

    InviteDetail

--- vue/src/components/InviteDetail.vue
+++ vue/src/components/InviteDetail.vue
@@ -13,20 +13,20 @@
               <b-button v-show="!this.invite.deadlinePassed" class="deadline-open">Voting Open</b-button>
               <b-button v-show="this.invite.deadlinePassed" class="deadline-close">Voting Closed</b-button>
           <div class="reservation-date">
-              <span class="res-head">Reservation Date and Time:</span><span> {{this.invite.reservationDate}}</span>
+              <span class="head">Reservation Date and Time:</span><span> {{this.invite.reservationDate}}</span>
           </div>
           <div class="invitees">
-          <span class="invitee-head">Participants:</span><span
+          <span class="head">Participants:</span><span
             v-for="invitee in this.friends"
             v-bind:key="invitee.uniqueId"
             > {{ invitee }} </span>
           </div>
           <div class="finalists">
-              <span class="finalist-head">Finalists:</span>
-              <div>
-              <restaurant-card v-for="restaurant in this.invite.businessDetails" :key="restaurant.id" :restaurant="restaurant" class="card"/>
-              <!--<span class="total-votes"> {{this.invite.businessDetails.totalThumbsUp}}/{{this.invite.invitees.length}}</span>-->
-              </div>
+              <span class="head">Finalists:</span>
+              
+              <restaurant-card v-for="restaurant in this.invite.businessDetails" :key="restaurant.id" :restaurant="restaurant" class="card">
+                  <span class="head">Total Votes:</span><span class="total-votes"> {{this.invite.businessDetails.totalThumbsUp}}/{{this.invite.invitees.length}}</span></restaurant-card>
+              
           </div>
           
 
@@ -55,23 +55,45 @@ export default {
     InviteService.getInviteById(this.$route.params.id).then((response) => {
       this.invite = response.data;
       this.isLoading = false;
-      for (let j = 0; j < this.invite.invitees.length; j++) {
-          this.friends.push(this.invite.invitees[j].name);
-          if (j < this.invite.invitees.length - 1) {
-            this.friends[j] += ", ";
-          } 
-        }
-      console.log(this.invite);
+      //for (let j = 0; j < this.invite.invitees.length; j++) {
+        //  this.friends.push(this.invite.invitees[j].name);
+          //if (j < this.invite.invitees.length - 1) {
+            //this.friends[j] += ", ";
+         // } 
+      //  }
+      
     });
   },
 };
 </script>
 
-<style>
+<style scoped>
 .deadline-open {
   background-color: green;
 }
 .deadline-close {
   background-color: red;
 }
+.main {
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    padding:100px;
+}
+
+
+h1{
+    font-size: 2.7vw;
+    text-align:center;
+
+
+}
+
+.card{
+    padding-top:20px;
+    margin-left:50px;
+    margin-right:50px;
+    
+}
+
 </style>
