aa78872a927887fdaacae0e2fe0de5b29552ee45
Joshua Bannen <josh.bannen@gmail.com>
2021-03-17 20:18:58 -0400

    detail

--- vue/src/components/InviteDetail.vue
+++ vue/src/components/InviteDetail.vue
@@ -1,39 +1,56 @@
 <template>
-  <div class="main" v-if="!isLoading">
-      <div class="left-panel">
+  <main>
+    <div class="main">
+      <div class="loading-gif" v-if="isLoading">
+        <img src="../assets/loading.gif" />
       </div>
-      <div class="right-panel">
-          <div class="invite">
-              <h1 class="invite-name">{{invite.inviteName}}</h1>
-          </div>
-          <div class="deadline">
-              <span class="deadline-head">Voting Deadline:</span><span> {{invite.deadline}}</span>
-          </div>
-          <div class="deadline-passed">
-              <b-button v-show="!this.invite.deadlinePassed" class="deadline-open">Voting Open</b-button>
-              <b-button v-show="this.invite.deadlinePassed" class="deadline-close">Voting Closed</b-button>
+      <div class="left-panel">
+        <div class="invite">
+          <h1 class="invite-name">{{ this.invite.inviteName }}</h1>
+        </div>
+        <div class="deadline">
+          <span class="deadline-head">Voting Deadline:</span
+          ><span> {{ this.invite.deadline }}</span>
+        </div>
+        <div class="deadline-passed">
+          <b-button v-show="!this.invite.deadlinePassed" class="deadline-open"
+            >Voting Open</b-button
+          >
+          <b-button v-show="this.invite.deadlinePassed" class="deadline-close"
+            >Voting Closed</b-button
+          >
           <div class="reservation-date">
-              <span class="head">Reservation Date and Time:</span><span> {{invite.reservationDate}}</span>
+            <span class="head">Reservation Date and Time:</span
+            ><span> {{ this.invite.reservationDate }}</span>
           </div>
           <div class="invitees">
-          <span class="head">Participants:</span><span
-            v-for="invitee in this.friends"
-            v-bind:key="invitee.uniqueId"
-            > {{ invitee }} </span>
+            <span class="head">Participants:</span
+            ><span
+              v-for="invitee in this.friends"
+              v-bind:key="invitee.uniqueId"
+            >
+              {{ invitee }}
+            </span>
           </div>
+        </div>
+        <div class="right-panel">
           <div class="finalists">
-              <span class="head">Finalists:</span>
-              
-              <restaurant-card v-for="restaurant in this.invite.businessDetails" :key="restaurant.id" :restaurant="restaurant" class="card"/>
-                  <span class="head">Total Votes:</span><span class="total-votes"> {{this.invite.businessDetails.totalThumbsUp}}/{{this.friends.length}}</span>
-              
+            <span class="head">Finalists:</span>
+            <restaurant-card
+              v-for="restaurant in this.invite.businessDetails"
+              :key="restaurant.id"
+              :restaurant="restaurant"
+              class="card"
+            />
+            <span class="head">Total Votes:</span
+            ><span class="total-votes">
+              {{ this.invite.totalThumbsUp }}/{{ this.friends.length }}</span
+            >
           </div>
-          
-
-
-          </div>         
+        </div>
       </div>
-  </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -49,27 +66,22 @@ export default {
       isLoading: true,
       friends: [],
       businessDetails: [],
-      
+
       /*invite.businessDetails loop to get businesses*/
     };
   },
   created() {
     InviteService.getInviteById(this.$route.params.id).then((response) => {
       this.invite = response.data;
-      console.log(this.invite);
-       console.log("array" + this.invite.invitees);
-      
-        for (let j = 0; j < this.invite.invitees.length; j++) {
-          this.friends.push(this.invite.invitees[j].name);
-          if (j < this.invite.invitees.length - 1) {
-            this.friends[j] += ", ";
-          }   
+      this.isLoading = true;
+      for (let j = 0; j < this.invite.invitees.length; j++) {
+        this.friends.push(this.invite.invitees[j].name);
+        if (j < this.invite.invitees.length - 1) {
+          this.friends[j] += ", ";
         }
-        this.businessDetails = this.invite.businessDetails;
-       this.isLoading = false;
-       console.log(this.invite.invitees);
-       console.log(this.invite.invitees);
-      
+      }
+      this.businessDetails = this.invite.businessDetails;
+      this.isLoading = false;
     });
   },
 };
@@ -83,25 +95,31 @@ export default {
   background-color: red;
 }
 .main {
-    display:flex;
-    flex-direction: column;
-    justify-content:center;
-    padding:100px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.left-panel {
+  padding: 20px;
+  margin-right: 25px;
+}
+.right-panel {
+  padding-top: 4.5em;
 }
 
-
-h1{
-    font-size: 2.7vw;
-    text-align:center;
-
-
+h1 {
+  font-size: 2.7vw;
+  text-align: center;
 }
-
-.card{
-    padding-top:20px;
-    margin-left:50px;
-    margin-right:50px;
-    
+.head {
+  font-size: 18pt;
+  font-weight: bold;
 }
-
+.card {
+  padding-top: 20px;
+  margin-left: 50px;
+  margin-right: 50px;
+}
 </style>
