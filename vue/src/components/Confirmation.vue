5e46c42aaf9cf251cbfda5b6f5c9406770ce0542
Michael Uhlman <muhlman@gmail.com>
2021-03-18 22:07:19 -0400

    confirmation
    Signed-off-by: Michael Uhlman <muhlman@gmail.com>

--- vue/src/components/Confirmation.vue
+++ vue/src/components/Confirmation.vue
@@ -88,9 +88,11 @@ export default {
   },
   created() {
     this.invitation = this.$store.state.createdInvite;
+    // DELETE the api call below.
     InviteService.getInviteById(1).then((response) => {
       this.invitation = response.data;
     });
+    // DELETE ABOVE
   },
   methods: {
     copyUrl(uniqueId) {
